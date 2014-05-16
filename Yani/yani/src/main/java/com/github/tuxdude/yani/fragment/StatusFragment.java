eaee820d996e9985b5f0b0ffa96afa5a66a39120
Ash <tuxdude@IronHide>
2014-05-16 01:16:31 -0700

    Print SSID info for the Status fragment.

--- Yani/yani/src/main/java/com/github/tuxdude/yani/fragment/StatusFragment.java
+++ Yani/yani/src/main/java/com/github/tuxdude/yani/fragment/StatusFragment.java
@@ -1,9 +1,12 @@
 package com.github.tuxdude.yani.fragment;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.tuxdude.yani.R;
 
@@ -17,6 +20,18 @@ public class StatusFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_yani, container, false);
+
+        TextView tv = (TextView)rootView.findViewById(R.id.section_label);
+        if (tv != null) {
+            tv.append("HELLO!\n");
+
+            WifiManager wifiManager = (WifiManager)getActivity().getSystemService(Context.WIFI_SERVICE);
+
+            android.net.wifi.WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+            tv.append(wifiInfo.getSSID() + "\n");
+        }
+
         return rootView;
     }
 }
