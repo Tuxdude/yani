a6be8cef89f213a583b28b823fbb5fc3c7708043
Ash <tuxdude@IronHide>
2014-05-16 18:06:50 -0700

    Use ConnectivityManager to populate status.

--- Yani/yani/src/main/java/com/github/tuxdude/yani/fragment/StatusFragment.java
+++ Yani/yani/src/main/java/com/github/tuxdude/yani/fragment/StatusFragment.java
@@ -1,7 +1,9 @@
 package com.github.tuxdude.yani.fragment;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.DhcpInfo;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -35,32 +37,42 @@ public class StatusFragment extends BaseFragment {
         TextView tv = (TextView)rootView.findViewById(R.id.section_label);
         if (tv != null) {
             tv.append("Connection Status\n");
+            ConnectivityManager connectivityManager = (ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-            WifiManager wifiManager = (WifiManager)getActivity().getSystemService(Context.WIFI_SERVICE);
-
-            if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
-
-                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-                if (wifiInfo != null) {
-                    tv.append("SSID: " + wifiInfo.getSSID() + "\n");
-                    tv.append("Link Speed: " + wifiInfo.getLinkSpeed() + " " + WifiInfo.LINK_SPEED_UNITS + "\n");
-                    tv.append("IP Address: " + new IpAddress(wifiInfo.getIpAddress()) + "\n");
-                    tv.append("MAC Address: " + wifiInfo.getMacAddress() + "\n");
-                }
-
-                DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
-                if (dhcpInfo != null) {
-                    tv.append("DHCP Info:\n");
-                    tv.append("IP Address: " + new IpAddress(dhcpInfo.ipAddress) + "\n");
-                    tv.append("Netmask: " + new IpAddress(dhcpInfo.netmask) + "\n");
-                    tv.append("Gateway: " + new IpAddress(dhcpInfo.gateway) + "\n");
-                    tv.append("DNS1: " + new IpAddress(dhcpInfo.dns1) + "\n");
-                    tv.append("DNS2: " + new IpAddress(dhcpInfo.dns2) + "\n");
-                    tv.append("DHCP Server IP: " + new IpAddress(dhcpInfo.serverAddress) + "\n");
+            if (connectivityManager != null) {
+                NetworkInfo networkInfoList[] = connectivityManager.getAllNetworkInfo();
+                if (networkInfoList != null) {
+                    for (NetworkInfo networkInfo : networkInfoList) {
+                        tv.append(networkInfo.toString() + "\n");
+                    }
                 }
             }
-            else {
-                tv.append("Wifi is not enabled\n");
+
+            WifiManager wifiManager = (WifiManager)getActivity().getSystemService(Context.WIFI_SERVICE);
+            if (wifiManager != null) {
+                if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
+
+                    WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                    if (wifiInfo != null) {
+                        tv.append("SSID: " + wifiInfo.getSSID() + "\n");
+                        tv.append("Link Speed: " + wifiInfo.getLinkSpeed() + " " + WifiInfo.LINK_SPEED_UNITS + "\n");
+                        tv.append("IP Address: " + new IpAddress(wifiInfo.getIpAddress()) + "\n");
+                        tv.append("MAC Address: " + wifiInfo.getMacAddress() + "\n");
+                    }
+
+                    DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
+                    if (dhcpInfo != null) {
+                        tv.append("DHCP Info:\n");
+                        tv.append("IP Address: " + new IpAddress(dhcpInfo.ipAddress) + "\n");
+                        tv.append("Netmask: " + new IpAddress(dhcpInfo.netmask) + "\n");
+                        tv.append("Gateway: " + new IpAddress(dhcpInfo.gateway) + "\n");
+                        tv.append("DNS1: " + new IpAddress(dhcpInfo.dns1) + "\n");
+                        tv.append("DNS2: " + new IpAddress(dhcpInfo.dns2) + "\n");
+                        tv.append("DHCP Server IP: " + new IpAddress(dhcpInfo.serverAddress) + "\n");
+                    }
+                } else {
+                    tv.append("Wifi is not enabled\n");
+                }
             }
         }
 
