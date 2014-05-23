71885fe18a80502c9274ae455c3bd66399fbc8b2
Ash <tuxdude@IronHide>
2014-05-23 01:00:42 -0700

    Disable toasts for network broadcast events.

--- Yani/yani/src/main/java/com/github/tuxdude/yani/network/NetworkBroadcastListener.java
+++ Yani/yani/src/main/java/com/github/tuxdude/yani/network/NetworkBroadcastListener.java
@@ -16,15 +16,15 @@ public class NetworkBroadcastListener extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Logger.trace();
         if (intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
-            Toast.makeText(context, "Airplane Mode Changed!", Toast.LENGTH_LONG).show();
+            //Toast.makeText(context, "Airplane Mode Changed!", Toast.LENGTH_LONG).show();
             Logger.d("Airplane Mode Changed");
         }
         else if (intent.getAction().equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
-            Toast.makeText(context, "Wifi State Changed!", Toast.LENGTH_LONG).show();
+            //Toast.makeText(context, "Wifi State Changed!", Toast.LENGTH_LONG).show();
             Logger.d("Wifi State Changed");
         }
         else if (intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
-            Toast.makeText(context, "Network State Changed!", Toast.LENGTH_LONG).show();
+            //Toast.makeText(context, "Network State Changed!", Toast.LENGTH_LONG).show();
             Logger.d("Network State Changed");
         }
         else {
